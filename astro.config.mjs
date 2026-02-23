0c6753cb4a3c0297e5f0dc684ec470ab3cec7228
yelishgiri <118233081+yelishgiri@users.noreply.github.com>
2026-02-23 09:25:42 -0600

    Add The Image Compression Script To Documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,39 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'RHCJC',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Overview', slug: 'guides/overview' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Plugins',
-					autogenerate: { directory: 'plugins' },
-				},
-				{
-					label: 'Designs',
-					autogenerate: { directory: 'designs' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "RHCJC",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ label: "Overview", slug: "guides/overview" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Plugins",
+          autogenerate: { directory: "plugins" },
+        },
+        {
+          label: "Designs",
+          autogenerate: { directory: "designs" },
+        },
+        {
+          label: "Scripts",
+          autogenerate: { directory: "scripts" },
+        },
+      ],
+    }),
+  ],
 });
